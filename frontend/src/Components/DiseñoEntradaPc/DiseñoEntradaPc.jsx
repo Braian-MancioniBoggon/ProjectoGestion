ba2b050cbd2adfadd25ddffadd995bdcb46ab8cb
Braian <mancioniboggon@gmail.com>
2024-07-16 17:01:09 -0300

    cambio de color en la medalla de estado del pedido

--- frontend/src/Components/DiseñoEntradaPc/DiseñoEntradaPc.jsx
+++ frontend/src/Components/DiseñoEntradaPc/DiseñoEntradaPc.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
-import { VStack, Flex, IconButton, Img, Box, Text, Heading, Input, Checkbox, Divider, Badge, Link } from '@chakra-ui/react';
+import { VStack, Flex, IconButton, Img, Box, Text, Heading, Input, Checkbox, Divider, Badge, Link, Tooltip } from '@chakra-ui/react';
 import { IconContext } from 'react-icons';
 import { MdOutlineAddBox, MdOutlineDriveFileRenameOutline, MdOutlineRemoveRedEye, MdDeleteOutline, MdOutlineFindInPage, MdOutlineFileCopy, MdOutlineWhatsapp } from "react-icons/md";
 import { TbReceiptDollar } from "react-icons/tb";
 
 const DiseñoEntradaPc = ({pedido, handleEditClick, borrarPedido, verPedido}) => {
     const [whatsappLink, setWhatsappLink] = useState(`https://wa.me/+54${pedido.telefono}`);
+
+    const colorEstadoPedido = (estadoPedido) => {
+        switch (estadoPedido) {
+            case "En proceso":
+                return "purple";
+            case "Finalizado":
+                return "green";
+            default:
+                return "red";
+        }
+      }
+
     return(
         <IconContext.Provider value={{ style: { verticalAlign: 'middle', fill:"", fontSize:"22px" } }}>
             <VStack w={"100%"}>
@@ -13,7 +25,7 @@ const DiseñoEntradaPc = ({pedido, handleEditClick, borrarPedido, verPedido}) =>
                 <Flex w={"100%"} direction={'row'}>
                     <Flex direction={'column'} alignItems={"start"} flexGrow={1} p={5}>
                         <Flex w={"max-content"} fontSize={"14px"}>{pedido.fecha}</Flex>
-                        <Flex w={"max-content"} fontSize={"20px"} fontWeight={"bolder"} lineHeight={"22px"} maxW={"110px"} p={"4px 0px"}>{pedido.nombre}</Flex>
+                        <Flex w={"max-content"} fontSize={"20px"} fontWeight={"bolder"} lineHeight={"22px"} minW={"175px"} maxW={"175px"} p={"4px 0px"}>{pedido.nombre}</Flex>
                         <Flex w={"max-content"} maxW={"110px"}>{pedido.telefono}</Flex>
                     </Flex>
                     <Flex direction={"column"} alignItems={"start"} flexGrow={50} p={5}>
@@ -25,19 +37,27 @@ const DiseñoEntradaPc = ({pedido, handleEditClick, borrarPedido, verPedido}) =>
                         <Flex w={"max-content"} justifyContent={"flex-end"}>Saldo: ${pedido.total - pedido.seña}</Flex>
                     </Flex>
                     <Flex direction={"column"} alignItems={"center"} flexGrow={1} p={5}>
-                        <Flex w={"max-content"} justifyContent={"center"} >Factura <Checkbox isDisabled isChecked={pedido.factura} ml={"5px"}></Checkbox></Flex>
-                        <Flex w={"max-content"} justifyContent={"center"} p={"6px 0px"}><Badge variant='solid' colorScheme='green'>{pedido.estado}</Badge></Flex>
+                        <Flex w={"max-content"} justifyContent={"center"} >Factura <Checkbox  isChecked={pedido.factura} ml={"5px"} colorScheme='green'></Checkbox></Flex>
+                        <Flex w={"max-content"} justifyContent={"center"} p={"6px 0px"}><Badge variant='solid' colorScheme={colorEstadoPedido(pedido.estado)}>{pedido.estado}</Badge></Flex>
                     </Flex>
                     <Flex direction={"column"} alignItems={"center"} justifyContent={"center"} flexGrow={1} p={5}>
                         <Flex direction={'row'}>
-                            <IconButton isRound='true' icon={<MdOutlineDriveFileRenameOutline />} onClick={() => handleEditClick(pedido)}  color="" bg="" _hover={{ bg:"" }}></IconButton>
-                            <Link href={whatsappLink} isExternal>
-                                <IconButton isRound='true' icon={<MdOutlineWhatsapp />}  color="" bg="" _hover={{ bg:"" }} ></IconButton>
-                            </Link>
+                            <Tooltip label='Editar' placement='top' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<MdOutlineDriveFileRenameOutline />} onClick={() => handleEditClick(pedido)}  color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='Enviar mensaje' placement='top' openDelay={250} hasArrow>
+                                <Link href={whatsappLink} isExternal>
+                                    <IconButton isRound='true' icon={<MdOutlineWhatsapp />}  color="" bg="" _hover={{ bg:"" }} ></IconButton>
+                                </Link>
+                            </Tooltip>
                         </Flex>
                         <Flex direction={'row'}>
-                            <IconButton isRound='true' icon={<TbReceiptDollar />} onClick={() => verPedido(pedido)} color="" bg="" _hover={{ bg:"" }}></IconButton>
-                            <IconButton isRound='true' icon={<MdDeleteOutline />} color="" bg="" _hover={{ bg:"" }} onClick={() => borrarPedido(pedido.nombre)}></IconButton>
+                            <Tooltip label='Ver' placement='bottom' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<TbReceiptDollar />} onClick={() => verPedido(pedido)} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='Eliminar' placement='bottom' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<MdDeleteOutline />} color="" bg="" _hover={{ bg:"" }} onClick={() => borrarPedido(pedido.nombre)}></IconButton>
+                            </Tooltip>
                         </Flex>
                     </Flex>
                 </Flex>
--- frontend/src/Components/Header/Header.jsx
+++ frontend/src/Components/Header/Header.jsx
@@ -1,30 +1,51 @@
 import React from 'react'
 import { MdOutlineWhatsapp, MdStorefront, MdInsertChartOutlined, MdFolderOpen, MdFacebook, MdSunny, MdBedtime } from "react-icons/md";
 import { RiInstagramLine } from "react-icons/ri";
-import { VStack, Flex, Img, Box, IconButton, useColorMode, Text, Heading } from '@chakra-ui/react'
+import { VStack, Flex, Img, Box, IconButton, useColorMode, Text, Heading, Tooltip } from '@chakra-ui/react'
 import { IconContext } from 'react-icons';
 
 const Header = ({esOscuro, toggleColorMode}) => {
     
     return(
-        <IconContext.Provider value={{ style: { verticalAlign: 'middle', fill:"", fontSize:"30px" } }}>
-        <VStack h="100vh" w="40px" justifyContent={"center"} pl={{base:"0px", sm:"0px",  md:"30px"}}  pr={{base:"0px", sm:"0px",  md:"30px"}} >
-                <Flex pos={"fixed"} direction={"column"} bgColor="" h="100vh" w="30px" borderRightRadius={"15px"} alignItems="center" justifyContent='space-between' pt="25px" pb="25px" pl={{base:"0px", sm:"0px",  md:"30px"}}  pr={{base:"0px", sm:"0px",  md:"30px"}} boxShadow={"-10px 0px 25px 5px rgba(0,0,0,0.75)"}>
-                    
+        <IconContext.Provider value={{ style: { verticalAlign: 'middle', fill:"", fontSize:"24px" } }}>
+        <VStack h="100vh" w="60px" justifyContent={"center"} alignItems="flex-start" >
+                <Flex pos={"fixed"} direction={"column"} bgColor="" h="100vh" w="30px" borderRightRadius={"15px"} alignItems="center" justifyContent='space-between' pt="25px" pb="25px" pl={"30px"}  pr={"30px"} boxShadow={"-10px 0px 25px 5px rgba(0,0,0,0.75)"}>
                     <Flex direction={'column'} grow={1} W={"40px"} justifyContent='space-between'> 
-                        <IconButton isRound='true' color="" bg="" _hover={{ bg:"" }}>
-                            <Img h="50px" w="auto" src={esOscuro ? "img/monogramaBlanco.png" : "img/monogramaRojo.png"} />
-                        </IconButton> 
-                        <IconButton isRound='true' color="" bg="" _hover={{ bg:"" }}>
-                            <Img h="30px" src={esOscuro ? "img/logoVari.png" : "img/logoVariAlt.png"} />
-                        </IconButton>
-                        <IconButton isRound='true' icon={<MdInsertChartOutlined />} color="" bg="" _hover={{ bg:"" }}></IconButton>
-                        <IconButton isRound='true' icon={<MdFolderOpen />} color="" bg="" _hover={{ bg:"" }}></IconButton>
-                        <IconButton isRound='true' icon={<MdStorefront />} color="" bg="" _hover={{ bg:"" }}></IconButton>
-                        <IconButton isRound='true' icon={<MdOutlineWhatsapp />} color="" bg="" _hover={{ bg:"" }}></IconButton>
-                        <IconButton isRound='true' icon={<RiInstagramLine />} color="" bg="" _hover={{ bg:"" }}></IconButton>
-                        <IconButton isRound='true' icon={<MdFacebook />} color="" bg="" _hover={{ bg:"" }}></IconButton>
-                        <IconButton  ml="" isRound='true' icon={esOscuro ? <MdSunny /> : <MdBedtime />} onClick={toggleColorMode} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                        <Flex direction={"column"} >
+                            <IconButton isRound='true' color="" bg="" _hover={{ bg:"" }}>
+                                <Img h="50px" w="auto" src={esOscuro ? "img/monogramaBlanco.png" : "img/monogramaNegro.png"} />
+                            </IconButton> 
+                        </Flex>
+                        <Flex direction={"column"} >
+                            <Tooltip label='Ecxel' placement='right' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<MdInsertChartOutlined />} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='Drive' placement='right' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<MdFolderOpen />} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='MercadoLibre' placement='right' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<MdStorefront />} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='Whatsapp' placement='right' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<MdOutlineWhatsapp />} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='Instagram' placement='right' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<RiInstagramLine />} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='Facebook' placement='right' openDelay={250} hasArrow>
+                                <IconButton isRound='true' icon={<MdFacebook />} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                            <Tooltip label='Vari' placement='right' openDelay={250} hasArrow>
+                                <IconButton isRound='true' color="" bg="" _hover={{ bg:"" }}>
+                                    <Img h="24px" src={esOscuro ? "img/logoVari.png" : "img/logoVariAlt.png"} />
+                                </IconButton>
+                            </Tooltip>
+                        </Flex>
+                        <Flex direction={"column"} >
+                            <Tooltip label={esOscuro ? "Modo claro" : "Modo oscuro"} placement='right' openDelay={250} hasArrow>
+                                <IconButton  ml="" isRound='true' icon={esOscuro ? <MdSunny /> : <MdBedtime />} onClick={toggleColorMode} color="" bg="" _hover={{ bg:"" }}></IconButton>
+                            </Tooltip>
+                        </Flex>
                     </Flex>
                 </Flex>
         </VStack>
